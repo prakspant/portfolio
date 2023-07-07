bd51791c782acf70d4b527509a007509203f1767
Prakhar Pant <prakharpant288@gmail.com>
2023-07-07 19:01:12 +0530

    my final version of the website

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,32 +1,32 @@
 import './index.scss';
 import LogoPTransparent from '../../../assets/images/LogoPTransparent.png';
 import { useRef, useEffect } from 'react';
-import gsap from 'gsap-trial';
+// import gsap from 'gsap-trial';
 import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
 
 const Logo = () => {
   const bgRef = useRef();
   const outlineLogoRef = useRef();
   const solidLogoRef = useRef();
-
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-
-    const tl = gsap.timeline();
-
-    tl.to(bgRef.current, {
-      duration: 1,
-      opacity: 1
-    })
-    .from(outlineLogoRef.current, {
-      drawSVG: 0,
-      duration: 2
-    })
-    .to(solidLogoRef.current, {
-      opacity: 0.3,
-      duration: 2
-    }, '-=1'); // this will start 1 second before the previous animation ends
-  }, [])
+  // 
+  // useEffect(() => {
+  //   gsap.registerPlugin(DrawSVGPlugin)
+  //
+  //   const tl = gsap.timeline();
+  //
+  //   tl.to(bgRef.current, {
+  //     duration: 1,
+  //     opacity: 1
+  //   })
+  //   .from(outlineLogoRef.current, {
+  //     drawSVG: 0,
+  //     duration: 2
+  //   })
+  //   .to(solidLogoRef.current, {
+  //     opacity: 0.3,
+  //     duration: 2
+  //   }, '-=1'); // this will start 1 second before the previous animation ends
+  // }, [])
 
   return (
     <div className='logo-container' ref={bgRef}>
